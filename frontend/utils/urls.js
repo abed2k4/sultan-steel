e38dca2e609c7306d6e54fc6539aef475bb5bc53
abed2k4 <abed2k4@gmail.com>
2023-04-15 13:10:21 +0300

    first

--- frontend/utils/urls.js
+++ frontend/utils/urls.js
@@ -2,3 +2,4 @@ export const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
 export const API_URL =
     process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:1337";
+    //instead of using the default https://127.0.0.1:1337
